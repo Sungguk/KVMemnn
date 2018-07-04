e9c1021e1a6888b501a68e152907677f73fc8dbc
Debayan Banerjee <debayanin@gmail.com>
2018-07-04 23:40:56 +0200

    Broken commit

--- model/memnn.py
+++ model/memnn.py
@@ -21,7 +21,7 @@ def reshaped(tensor,batch_size,pad_length,seq_length):
     return tensor
 
 class KVMMModel(nn.Module):
-    def __init__(self, pad_length=20,batch_size=100,embedding_size=200,n_chars=20,vocab_size=1000,n_labels=20,encoder_units=256,decoder_units=256):
+    def __init__(self, pad_length=20,batch_size=1,embedding_size=200,n_chars=20,vocab_size=1000,n_labels=20,encoder_units=256,decoder_units=256):
         super(KVMMModel, self).__init__()
         self.pad_length = pad_length
         self.batch_size = batch_size
@@ -31,35 +31,53 @@ class KVMMModel(nn.Module):
         self.n_labels =  n_labels
         self.encoder_units = encoder_units
         self.decoder_units = decoder_units
+      
+        self.input_embed_dialogues = nn.Embedding(self.vocab_size, self.embedding_size, self.pad_length)
+        self.dialogue_dropout = nn.Dropout(0.2)
+        self.encoder_dialogue = nn.LSTM(self.embedding_size, self.embedding_size)
+        self.decoder_dialogue = nn.LSTM(self.embedding_size, self.embedding_size)
+        self.dense1_dialogue = nn.Sequential(nn.Linear(200,200),nn.Tanh())
+        self.dense2_dialogue = nn.Sequential(nn.Linear(200,200),nn.Tanh())
+        self.dense3_dialogue = nn.Sequential(nn.Linear(200,200),nn.Tanh())
+        self.dialogue_output = nn.Linear(400,1954)
 
-    def forward(self, input_dialogues, input_keyvalues):
+        self.input_embed_keyvalue = nn.Embedding(self.vocab_size, self.embedding_size,431)
+        self.keyvalue_dense1 = nn.Sequential(nn.Linear(200,20),nn.Tanh())
+        self.keyvalue_dense2 = nn.Sequential(nn.Linear(20,200),nn.Tanh())
+        self.keyvalue_dense3 = nn.Sequential(nn.Linear(40,431),nn.Tanh())
+
+    def forward(self, input_dialogue, input_keyvalues):
         #input1: Dialogues
-        input_embed1 = nn.Embedding(self.vocab_size, self.embedding_size, self.pad_length)(input_dialogues)
-        dropout = nn.Dropout(0.2)(input_embed1)
-        encoder = nn.LSTM(self.encoder_units)(dropout)
-        decoder = nn.LSTM(self.decoder_units)(encoder)
-        dense1 = nn.Sequential(nn.Linear(200),nn.tanh())(encoder)
-        dense2 = nn.Sequential(nn.Linear(200),nn.tanh())(decoder)
-        dense3 = nn.Sequential(nn.Linear(200),nn.tanh())(torch.add(dense1, dense2)) #equation 2 (refer to https://arxiv.org/pdf/1705.05414.pdf)
+        input_embed1 = self.input_embed_dialogues(input_dialogue)
+        dropout = self.dialogue_dropout(input_embed1)
+        encoder = self.encoder_dialogue(dropout)
+        decoder = self.decoder_dialogue(encoder[0])
+        dense1 = self.dense1_dialogue(encoder[0])
+        dense2 = self.dense2_dialogue(decoder[0])
+        dense3 = self.dense3_dialogue(torch.add(dense1, dense2)) #equation 2 (refer to https://arxiv.org/pdf/1705.05414.pdf)
         attention = F.softmax(dense3) #equation 3
-        n_hidden = torch.mul(attention, encoder) #equation 4  
-        output = nn.Linear(1954)(torch.cat((encoder, n_hidden), dim=0)) #equation 5
+        n_hidden = torch.mul(attention, encoder[0]) #equation 4  
+        output = self.dialogue_output(torch.cat((encoder[0], n_hidden),dim=2)) #equation 5
     
         # input2: Key value table
-        input_embed2 = nn.Embedding(self.vocab_size, self.embedding_size,431)(input_keyvalues)
+        input_embed2 = self.input_embed_keyvalue(input_keyvalues)
+        print "input_embed",input_embed2.shape
         input_embed2 = reshape(input_embed2, self.batch_size, 431, self.embedding_size, self.pad_length)
-        decoder = reshaped(decoder, self.batch_size, self.pad_length, self.decoder_units)
-        n_dense1 = nn.Sequential(nn.Linear(20),nn.tanh())(input_embed2)
-        n_dense1 = reshaped(n_dense1, batch_size, self.pad_length,  self.decoder_units)
-        n_dense2 = nn.Sequential(nn.Linear(200),nn.tanh())(decoder)
-        n_dense3 = nn.Sequential(nn.Linear(431),nn.tanh())(torch.cat(n_dense1, n_dense2, 0)) #equation 7
+        print "input_embed",input_embed2.shape
+        n_dense1 = self.keyvalue_dense1(input_embed2)
+        print "n_dense1",n_dense1.shape
+        n_dense1 = reshaped(n_dense1, self.batch_size, self.pad_length,  self.decoder_units)
+        print "n_dense1",n_dense1.shape
+        decoder = reshaped(decoder[0], self.batch_size, self.pad_length, self.decoder_units)
+        n_dense2 = self.keyvalue_dense2(decoder)
+        print "n_dense2",n_dense2.shape
+        n_dense3 = self.keyvalue_dense3(torch.cat((n_dense1, n_dense2), 2)) #equation 7
+        print "n_dense3",n_dense3.shape
         n_dense3 = reshape2(n_dense3, self.batch_size,  pad_length, 431)
+        print "n_dense3",n_dense3.shape
         n_out = torch.add(output, n_dense3) # equation 8
         n_output = F.softmax(n_out) # equation 9
         return n_output
 
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
-
-
-
+    #def initHidden(self):
+    #    return torch.zeros(1, 1, self.hidden_size, device=device)
--- run.py
+++ run.py
@@ -2,21 +2,74 @@
     Runs a simple Neural Machine Translation model
     Type `python run.py -h` for help with arguments.
 """
-import os
+import os,sys
 import argparse
 import numpy as np
-import os
 import torch
+from torch import optim,nn
 from reader import Data,Vocabulary
 from model.memnn import KVMMModel
 from torchsummary import summary
+import random
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # create a directory if it doesn't already exist
 if not os.path.exists('./weights'):
     os.makedirs('./weights/')
 
+MAX_LENGTH = 20
+
+def train(input_tensor, target_tensor, kbs, model, model_optimizer, criterion, max_length=MAX_LENGTH):
+#    model_hidden = model.initHidden()
+
+    model_optimizer.zero_grad()
+
+    input_length = input_tensor.size
+    target_length = target_tensor.size
+
+
+    model_outputs = torch.zeros(max_length, 1, device=device)
+
+    loss = 0
+    input_tensor = torch.from_numpy(np.expand_dims(input_tensor,axis=0))
+    kbs = torch.from_numpy(np.expand_dims(kbs,axis=0))
+    output = model(input_tensor, kbs)
+#
+#    decoder_input = torch.tensor([[SOS_token]], device=device)
+#
+#    decoder_hidden = encoder_hidden
+#
+#    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+#
+#    if use_teacher_forcing:
+#        # Teacher forcing: Feed the target as the next input
+#        for di in range(target_length):
+#            decoder_output, decoder_hidden, decoder_attention = decoder(
+#                decoder_input, decoder_hidden, encoder_outputs)
+#            loss += criterion(decoder_output, target_tensor[di])
+#            decoder_input = target_tensor[di]  # Teacher forcing
+#
+#    else:
+#        # Without teacher forcing: use its own predictions as the next input
+#        for di in range(target_length):
+#            decoder_output, decoder_hidden, decoder_attention = decoder(
+#                decoder_input, decoder_hidden, encoder_outputs)
+#            topv, topi = decoder_output.topk(1)
+#            decoder_input = topi.squeeze().detach()  # detach from history as input
+#
+#            loss += criterion(decoder_output, target_tensor[di])
+#            if decoder_input.item() == EOS_token:
+#                break
+#
+#    loss.backward()
+#
+#    encoder_optimizer.step()
+#    decoder_optimizer.step()
+#    return loss.item() / target_length
+
+
 def main(args):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # Dataset functions
     vocab = Vocabulary('./data/vocabulary.json', padding=args.padding)
     vocab = Vocabulary('./data/vocabulary.json',
@@ -38,14 +91,22 @@ def main(args):
     model = KVMMModel(pad_length=args.padding,
                   embedding_size=args.embedding,
                   vocab_size=vocab.size(),
-                  batch_size=args.batch_size,
+                  batch_size=1,
                   n_chars=vocab.size(),
                   n_labels=vocab.size(),
                   encoder_units=200,
                   decoder_units=200).to(device)
 
+    print(model)
+    model_optimizer = optim.SGD(model.parameters(), lr=0.01)
+    criterion = nn.NLLLoss()
 
-    #run_examples(model, input_vocab, output_vocab)
+
+    for iter in range(1, 2):
+        input_tensor = training.inputs[iter-1]
+        target_tensor = training.targets[iter-1]
+        loss = train(input_tensor, target_tensor, training.kbs, model, model_optimizer, criterion)
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
