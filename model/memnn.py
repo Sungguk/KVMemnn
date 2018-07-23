4c5b1fbf5d2fa98d25ae08aa0a6fdf095b98eba9
m3i9b2 <m3i9b2@gmail.com>
2018-07-23 10:58:04 +0200

    remove print statement

--- model/memnn.py
+++ model/memnn.py
@@ -55,19 +55,19 @@ class KVMMModel(nn.Module):
             input_embed1 = self.input_embed_dialogues(input_dialogue.cuda())
         else:
             input_embed1 = self.input_embed_dialogues(input_dialogue)
-        #print "input_embed1",input_embed1.shape
+        
         dropout = self.dialogue_dropout(input_embed1)
-        #print "dropout",dropout.shape
+        
         encoder = self.encoder_dialogue(dropout)
-        #print "encoder",encoder[0].shape
+        
         decoder = self.decoder_dialogue(encoder[0])
-        #print "decoder",decoder[0].shape
+        
         dense1 = self.dense1_dialogue(encoder[0])
-        #print "dense1",dense1.shape
+        
         dense2 = self.dense2_dialogue(decoder[0])
-        #print "dense2",dense2.shape
+        
         dense3 = self.dense3_dialogue(torch.add(dense1, dense2)) #equation 2 (refer to https://arxiv.org/pdf/1705.05414.pdf)
-        #print "dense3",dense3.shape
+        
         attention = F.softmax(dense3, dim=2) #equation 3
         n_hidden = torch.mul(attention, encoder[0]) #equation 4  
         output = self.dialogue_output(torch.cat((encoder[0], n_hidden),dim=2)) #equation 5
@@ -78,25 +78,20 @@ class KVMMModel(nn.Module):
         else:
             input_embed2 = self.input_embed_keyvalue(input_keyvalues)
         input_embed2 = reshape(input_embed2, self.batch_size, 431, self.embedding_size, self.pad_length)
-        #print "input_embed2",input_embed2.shape
+        
         n_dense1 = self.keyvalue_dense1(input_embed2)
-        #print "n_dense1",n_dense1.shape
+        
         n_dense1 = reshaped(n_dense1, self.batch_size,  self.pad_length, self.decoder_units)
-        #print "n_dense1",n_dense1.shape
+        
         decoder = reshaped(decoder[0], self.batch_size, self.pad_length, self.decoder_units)
-        #print "decoder",decoder.shape
+        
         n_dense2 = self.keyvalue_dense2(decoder)
-        #print "n_dense2",n_dense2.shape
-        #print "concat",torch.cat((n_dense1, n_dense2), dim=1).shape
+        
         n_dense3 = self.keyvalue_dense3(torch.cat((n_dense1, n_dense2), dim=1)) #equation 7
-        #print "n_dense3",n_dense3.shape
+        
         n_dense3 = reshape2(n_dense3, self.batch_size,  self.pad_length, 431)
-        #print "n_dense3",n_dense3.shape
         n_out = torch.add(output, n_dense3) # equation 8
-        #print "n_out",n_out.shape
-        #n_output = F.softmax(n_out, dim=2) # equation 9
-        #print "n_output",n_output.shape
+        
         return n_out
 
-    #def initHidden(self):
-    #    return torch.zeros(1, 1, self.hidden_size, device=device)
+   
